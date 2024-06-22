cb2d8a7178ac0a838f5e904e30a0c40eb98f0cc3
faresmo2 <faresmohamed698@gmail.com>
2024-06-22 23:41:04 +0200

    update

--- components/ImageResult.jsx
+++ components/ImageResult.jsx
@@ -1,11 +1,11 @@
 import ImageComponent from "../reusable-components/ImageComponent";
 
-function ImageResult() {
+function ImageResult({ img }) {
   return (
     <li className="border border-gray-200 p-5 rounded-md">
-      <ImageComponent src="/sc-type.jpg" alt="result" />
-      <div className="text-center">
-        <h2 className="text-lg mb-6 font-semibold">
+      <ImageComponent src={img} alt="result" />
+      <div className="text-center mt-8">
+        <h2 className="text-lg mb-3 font-semibold">
           your type of illness is (type)
         </h2>
         <p className="text-lg">The Result is : (result)</p>

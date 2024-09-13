03d5e35eef365e2271b86dffa3f385db88edac1d
Fares Mohamed <faresmohamed698@gmail.com>
2024-09-13 06:05:19 +0300

    "deployment"

--- pages/SignUp.jsx
+++ pages/SignUp.jsx
@@ -53,7 +53,7 @@ function SignUp() {
           "Content-Type": "application/json",
         },
       })
-        .then((res) => {
+        .then(() => {
           toast.success("Registered Successfully");
           navigate("/login");
         })
@@ -63,29 +63,29 @@ function SignUp() {
     }
   }
   return (
-    <div className="m-8 flex gap-4">
-      <div className="w-2/5 h-full hidden lg:block">
+    <div className="flex gap-4 m-8">
+      <div className="hidden w-2/5 h-full lg:block">
         <ImageComponent
           src="/login.jpg"
           alt="loginPage"
-          className="h-full w-full object-cover rounded-3xl"
+          className="object-cover w-full h-full rounded-3xl"
         />
       </div>
-      <div className="w-full lg:w-3/5 mt-24">
+      <div className="w-full mt-24 lg:w-3/5">
         <div className="text-center">
           <h2 className="block antialiased tracking-normal font-sans text-4xl leading-[1.3] text-inherit font-bold mb-4">
             Join Us | SkinCare
           </h2>
-          <p className="block antialiased font-sans text-blue-gray-900 text-lg font-normal">
+          <p className="block font-sans text-lg antialiased font-normal text-blue-gray-900">
             Enter your name and email and password to register.
           </p>
         </div>
         <form
           onSubmit={handleSubmit}
-          className="mt-8 mb-2 mx-auto w-80 max-w-screen-lg lg:w-1/2"
+          className="max-w-screen-lg mx-auto mt-8 mb-2 w-80 lg:w-1/2"
         >
-          <div className="mb-1 flex flex-col gap-6">
-            <p className="block antialiased font-sans text-sm leading-normal text-blue-gray-900 -mb-3 font-medium">
+          <div className="flex flex-col gap-6 mb-1">
+            <p className="block -mb-3 font-sans text-sm antialiased font-medium leading-normal text-blue-gray-900">
               Your Name
             </p>
             <div className="relative w-full min-w-[200px] h-11">
@@ -98,7 +98,7 @@ function SignUp() {
               />
               <label className="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-blue-gray-500 leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[4.1] text-gray-500 peer-focus:text-gray-900 before:border-blue-gray-200 peer-focus:before:!border-gray-900 after:border-blue-gray-200 peer-focus:after:!border-gray-900 before:content-none after:content-none"></label>
             </div>
-            <p className="block antialiased font-sans text-sm leading-normal text-blue-gray-900 -mb-3 font-medium">
+            <p className="block -mb-3 font-sans text-sm antialiased font-medium leading-normal text-blue-gray-900">
               Your Email
             </p>
             <div className="relative w-full min-w-[200px] h-11">
@@ -111,7 +111,7 @@ function SignUp() {
               />
               <label className="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-blue-gray-500 leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[4.1] text-gray-500 peer-focus:text-gray-900 before:border-blue-gray-200 peer-focus:before:!border-gray-900 after:border-blue-gray-200 peer-focus:after:!border-gray-900 before:content-none after:content-none"></label>
             </div>
-            <p className="block antialiased font-sans text-sm leading-normal text-blue-gray-900 -mb-3 font-medium">
+            <p className="block -mb-3 font-sans text-sm antialiased font-medium leading-normal text-blue-gray-900">
               Password
             </p>
             <div className="relative w-full min-w-[200px] h-11">
@@ -137,14 +137,14 @@ function SignUp() {
                 checked={isChecked}
                 onChange={() => setIsChecked(!isChecked)}
               />
-              <span className="text-white absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 pointer-events-none opacity-0 peer-checked:opacity-100 transition-opacity">
+              <span className="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
                 <IoMdCheckmark className="h-3.5 w-3.5 my-2" />
               </span>
             </label>
-            <label className="text-gray-700 font-light select-none cursor-pointer mt-px">
-              <p className="antialiased font-sans text-sm leading-normal text-gray-700 flex items-center justify-start font-medium">
+            <label className="mt-px font-light text-gray-700 cursor-pointer select-none">
+              <p className="flex items-center justify-start font-sans text-sm antialiased font-medium leading-normal text-gray-700">
                 I agree the
-                <Link className="ml-2 font-normal text-black transition-colors hover:text-gray-900 underline">
+                <Link className="ml-2 font-normal text-black underline transition-colors hover:text-gray-900">
                   Terms and Conditions
                 </Link>
               </p>
